289410982da544ee8ec1876b5abc4c5bb1c4e9dd
m-ibarra-arg <m.ibarra@ing.unrc.edu.ar>
2020-04-26 20:30:42 -0300

    update doc/

--- app.py
+++ app.py
@@ -9,21 +9,25 @@ app.config['SECRET_KEY'] = 'secret!'
 pagedown = PageDown(app)
 
 
-class VentanaEdicion(FlaskForm):
+class WindowEdit(FlaskForm):
     pagedown = PageDownField()
     submit = SubmitField('Submit')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = VentanaEdicion()
+    form = WindowEdit()
     text = None
     if form.validate_on_submit():
         text = form.pagedown.data
     else:
-        form.pagedown.data = ('# This is demo #2 of Flask-PageDown\nThe '
-                               '*preview* is rendered separately from the '
-                               '*input*, and in this case it is located above.')
+        f = open('doc/my_file.md')
+        file_example = f.read()
+        form.pagedown.data = file_example
+        f.close()
+        # form.pagedown.data = ('# This is demo #2 of Flask-PageDown\nThe '
+        #                        '*preview* is rendered separately from the '
+        #                        '*input*, and in this case it is located above.')
     return render_template('index.html', form=form, text=text)
 
 
